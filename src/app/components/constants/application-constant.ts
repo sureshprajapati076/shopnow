1b8d6b052184d9fcf2f60aeb41000b01ff9f6d83
Suresh Prajapati <51094657+sureshprajapati076@users.noreply.github.com>
2020-04-15 12:46:20 -0500

    new added files

--- src/app/components/constants/application-constant.ts
+++ src/app/components/constants/application-constant.ts
@@ -23,27 +23,27 @@ export class ApplicationConstants {
     }
 }
 
-export class ApplicationConstants1 {
-    static readonly API_PATH = {
-        login: 'http://34.70.194.228:8055/signin',
-        signup: 'http://34.70.194.228:8055/signup',
-        getuserdetails: 'http://34.70.194.228:8055/get-user-detail',
-        updateuser: 'http://34.70.194.228:8055/update-user',
-        checkadmin: 'http://34.70.194.228:8055/checkifadmin',
-        getproducts: 'http://34.70.194.228:8055/products/getproducts',
-        getvendors: 'http://34.70.194.228:8055/products/getvendors',
-        getcartitems: 'http://34.70.194.228:8055/cart/get-cart-items',
-        getitembyid: 'http://34.70.194.228:8055/products/getbyid/',
-        addproduct: 'http://34.70.194.228:8055/products/addproduct',
-        addvendor: 'http://34.70.194.228:8055/products/addvendor',
-        addcomment: 'http://34.70.194.228:8055/comments/add',
-        addreply: 'http://34.70.194.228:8055/comments/reply/',
-        addtocart: 'http://34.70.194.228:8055/cart/add-product',
-        removefromcart: 'http://34.70.194.228:8055/cart/remove-product',
-        saveforlater: 'http://34.70.194.228:8055/cart/save-for-later',
-        putback2cart: 'http://34.70.194.228:8055/cart/put-back-to-cart',
-        makepayment: 'http://34.70.194.228:8055/cart/make-payment',
-        loadcomment: 'http://34.70.194.228:8055/comments/find/',
-        myorderhistory: 'http://34.70.194.228:8055/cart/my-order-list'
-    }
-}
+// export class ApplicationConstants1 {
+//     static readonly API_PATH = {
+//         login: 'http://34.70.194.228:8055/signin',
+//         signup: 'http://34.70.194.228:8055/signup',
+//         getuserdetails: 'http://34.70.194.228:8055/get-user-detail',
+//         updateuser: 'http://34.70.194.228:8055/update-user',
+//         checkadmin: 'http://34.70.194.228:8055/checkifadmin',
+//         getproducts: 'http://34.70.194.228:8055/products/getproducts',
+//         getvendors: 'http://34.70.194.228:8055/products/getvendors',
+//         getcartitems: 'http://34.70.194.228:8055/cart/get-cart-items',
+//         getitembyid: 'http://34.70.194.228:8055/products/getbyid/',
+//         addproduct: 'http://34.70.194.228:8055/products/addproduct',
+//         addvendor: 'http://34.70.194.228:8055/products/addvendor',
+//         addcomment: 'http://34.70.194.228:8055/comments/add',
+//         addreply: 'http://34.70.194.228:8055/comments/reply/',
+//         addtocart: 'http://34.70.194.228:8055/cart/add-product',
+//         removefromcart: 'http://34.70.194.228:8055/cart/remove-product',
+//         saveforlater: 'http://34.70.194.228:8055/cart/save-for-later',
+//         putback2cart: 'http://34.70.194.228:8055/cart/put-back-to-cart',
+//         makepayment: 'http://34.70.194.228:8055/cart/make-payment',
+//         loadcomment: 'http://34.70.194.228:8055/comments/find/',
+//         myorderhistory: 'http://34.70.194.228:8055/cart/my-order-list'
+//     }
+// }
